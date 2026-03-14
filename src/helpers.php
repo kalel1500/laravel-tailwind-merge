50f8df0ff35cdde3aefa9a83cdccc51b1a1e1af0
Adrian Canals <a.canals14@gmail.com>
2026-03-14 21:07:08 +0100

    (fix) arreglar tipado helper (twMerge)

--- src/helpers.php
+++ src/helpers.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 if (!function_exists('twMerge')) {
     /**
-     * @param  array<array-key, string|array<array-key, string>>  ...$args
+     * @param  string|array<array-key, string|array<array-key, string>>  ...$args
      */
     function twMerge(...$args): string
     {
